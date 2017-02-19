9a593d7ed3b49a8c2a19e64efe688a9e9744910e
mbahgoez <bagus.manto1@gmail.com>
2017-02-19 16:30:18 +0800

    fix koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,4 @@
 <?php
 include "functions.php";
-$db = new PDO("mysql:host=localhost:3306;dbname=dbmusic", "root", "");
+$db = new PDO("mysql:host=mysql.idhostinger.com;dbname=u945120114_music", "u945120114_music", "loloksemut48");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
