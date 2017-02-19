17ad574d085626cbb0795966733ecc9130ff6406
mbahgoez <bagus.manto1@gmail.com>
2017-02-19 16:35:38 +0800

    fix class

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php 
 
 require "vendor/autoload.php";
-
 // use Psr\Http\Message\ServerRequestInterface;
 // use Psr\Http\Message\ResponseInterface;
 
-$app = new \Slim\app;
+$app = new \Slim\App;
 
 // $app->add(function(ServerRequestInterface $request, ResponseInterface $response, callable $next){
 // 	return $next($request, $response);
